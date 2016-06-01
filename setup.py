bb4a269b4fecc5498a4ae315cd788a7519633f76
Mariano Mara <mmara@onapsis.com>
2016-06-01 19:08:15 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'colander',
+    'psycopg2',
+    'pyramid',
+    'pyramid_sqlalchemy',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    ]
+
+setup(name='multitenant_rls',
+      version='0.0',
+      description='multitenant_rls',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = multitenant_rls:main
+      [console_scripts]
+      initialize_multitenant_rls_db = multitenant_rls.scripts.initializedb:main
+      """,
+      )
